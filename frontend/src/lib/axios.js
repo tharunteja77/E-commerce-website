004b5e3ac52191f8fa04092fed206d4671b267b3
TharunTejaYarra <118092890+tharunteja77@users.noreply.github.com>
2025-06-23 21:58:12 +0530

    render axois changes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: import.meta.mode === "development" ? "http://localhost:5000/api" : "/api",
+	baseURL: import.meta.mode === "development" ? "http://localhost:5000/api" : import.meta.env.VITE_PRODUCTION_BASE_URL,
 	withCredentials: true, // send cookies to the server
 });
 
